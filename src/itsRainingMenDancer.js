bbe326a0c2b98aa71de38e48dfc977980f07d6ae
ryanjlee <ryanjlee12@gmail.com>
2014-12-15 19:55:10 -0800

    it is raining men

---
+++ src/itsRainingMenDancer.js
@@ -0,0 +1,20 @@
+var ItsRainingMenDancer = function(top, left, timeBetweenSteps) {
+  debugger;
+  Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node.addClass("itsRainingMenDancer");
+};
+
+
+ItsRainingMenDancer.prototype = Object.create(Dancer.prototype);
+ItsRainingMenDancer.prototype.constructor = ItsRainingMenDancer;
+
+ItsRainingMenDancer.prototype.oldStep = Dancer.prototype.step;
+ItsRainingMenDancer.prototype.step = function(){
+  this.oldStep();
+  this.top += 20;
+  this.setPosition(this.top, this.left);
+  setTimeout(function() {
+    this.top -= 20;
+    this.setPosition(this.top, this.left);
+  }, 100);
+};
