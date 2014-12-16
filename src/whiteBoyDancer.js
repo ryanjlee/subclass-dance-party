69896b9a12b6907bd4f3d837f92413202d2abf8d
ryanjlee <ryanjlee12@gmail.com>
2014-12-15 17:28:05 -0800

    progress on DanceFloor

--- src/whiteBoyDancer.js
+++ src/whiteBoyDancer.js
@@ -1,5 +1,21 @@
 var WhiteBoyDancer = function(top, left, timeBetweenSteps) {
   Dancer.call(this, top, left, timeBetweenSteps);
+  this.rightOrLeft = true;
+  this.$node = $('<span class = "whiteBoyDancer dancer"></span>')
 };
 
-WhiteBoyDancer.prototype = Object .create(Dan)
+WhiteBoyDancer.prototype = Object.create(Dancer.prototype);
+WhiteBoyDancer.prototype.constructor = WhiteBoyDancer;
+
+WhiteBoyDancer.prototype.oldStep = Dancer.prototype.step;
+
+WhiteBoyDancer.prototype.step = function() {
+  this.oldStep();
+  if(this.rightOrLeft) {
+    this.left += 25;
+  } else {
+    this.left -= 25;
+  }
+  this.rightOrLeft = !this.rightOrLeft;
+  //setPosition
+}
